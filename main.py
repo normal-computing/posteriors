f4e3e0281bb225db88b9b6864fc900d98d1379ea
Johnathan Chiu <johnathanchiu@berkeley.edu>
2023-09-28 18:09:12 -0400

    Add PL functionality for saving, etc.

--- main.py
+++ main.py
@@ -2,49 +2,77 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
 from pytorch_lightning.loggers import CSVLogger
 
+from sghmc.utils import parse_devices, setup_log_dir
 from sghmc.dataloader import ClincOOSDataLoader
 from sghmc.modules.transformer import MiniTransformer
 from sghmc.model import SGHMCModel
 
+import argparse
+import datetime
+import os
 
-# log_dir_name = "logs/train-1"
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--base", type=str)
+parser.add_argument("--resume", default=None, type=str)
+parser.add_argument("--devices", default=parse_devices, type=str)
+parser.add_argument("-d", "--debug", default=False, action="store_true")
+args = parser.parse_args()
 
-# logger = CSVLogger(name="csvlogger", save_dir=log_dir_name, version="")
-# callbacks = [
-#     TQDMProgressBar(refresh_rate=1),
-#     ModelCheckpoint(
-#         dirpath=f"{log_dir_name}/checkpoints/trainstep_checkpoints",
-#         filename="{epoch:06}-{step:09}",
-#         every_n_train_steps=10,
-#         save_last=True,
-#         verbose=True,
-#         save_weights_only=True,
-#     ),
-#     ModelCheckpoint(
-#         dirpath=f"{log_dir_name}/checkpoints",
-#         filename="{epoch:06}",
-#         verbose=True,
-#         save_last=True,
-#         save_on_train_epoch_end=True,
-#         save_weights_only=False,
-#     ),
-# ]
+timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+log_dir_name = setup_log_dir(
+    "logs",
+    timestamp,
+    debug=args.debug,
+    # resume=args.resume,
+)
 
 lr = 1e-1
-trainer_kwargs = {
-    "max_epochs": 100,
-    # "accelerator": "gpu",
-    "accelerator": "cpu",
-    # "devices": [3],
-    # "strategy": "ddp_find_unused_parameters_false",
-    "num_sanity_val_steps": 0,
-}
-
 
 if __name__ == "__main__":
-    data = ClincOOSDataLoader("data", batch_size=1000, shuffle=True, num_workers=8)
-    model = SGHMCModel(MiniTransformer(), lr=lr)
+    trainer_kwargs = {
+        "max_epochs": 100,
+        "accelerator": "cpu" if callable(args.devices) else "gpu",
+        "strategy": "auto",
+        "num_sanity_val_steps": 0,
+        "num_processes": 1,
+    }
 
+    if not callable(args.devices):
+        trainer_kwargs["devices"] = args.devices
+
+    # logger = CSVLogger(name="csvlogger", save_dir=log_dir_name, version="")
+    callbacks = [
+        TQDMProgressBar(refresh_rate=1),
+        ModelCheckpoint(
+            dirpath=f"{log_dir_name}/checkpoints/trainstep_checkpoints",
+            filename="{epoch:06}-{step:09}",
+            every_n_train_steps=10,
+            save_last=True,
+            verbose=True,
+            save_weights_only=True,
+        ),
+        ModelCheckpoint(
+            dirpath=f"{log_dir_name}/checkpoints",
+            filename="{epoch:06}",
+            verbose=True,
+            save_last=True,
+            save_on_train_epoch_end=True,
+            save_weights_only=False,
+        ),
+    ]
+    trainer_kwargs["callbacks"] = callbacks
+
+    dataset = ClincOOSDataLoader("data", batch_size=1000, shuffle=True, num_workers=8)
+    model = SGHMCModel(MiniTransformer(), lr=lr)
     trainer = pl.Trainer(**trainer_kwargs)
-    trainer.fit(model, data)
+
+    try:
+        # resume_ckpt = None
+        # if args.resume is not None:
+        #     resume_ckpt = os.path.join(args.resume, "checkpoints", "last.ckpt")
+        trainer.fit(model, dataset, ckpt_path=None)
+    finally:
+        if trainer.global_rank == 0:
+            final_ckpt = os.path.join(log_dir_name, "checkpoints", "last.ckpt")
+            trainer.save_checkpoint(final_ckpt)
--- sghmc/model.py
+++ sghmc/model.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from .optimizer.optimizer import SGHMC
 
 import pytorch_lightning as pl
@@ -9,6 +10,8 @@ class SGHMCModel(pl.LightningModule):
         super().__init__()
         self.automatic_optimization = False
 
+        self.save_parameters = []
+
         self.model = model
         self.lr = lr
 
@@ -17,6 +20,15 @@ class SGHMCModel(pl.LightningModule):
         outputs = self.model(x)
         loss = F.cross_entropy(outputs, y.squeeze(-1))
         return loss
+
+    def on_save_checkpoint(self, checkpoint: Dict[str, Any]) -> None:
+        checkpoint["save_params"] = self.save_parameters
+        return super().on_save_checkpoint(checkpoint)
+
+    def on_after_backward(self) -> None:
+        params = [p.detach().cpu().numpy() for p in list(self.model.parameters())]
+        self.save_parameters = self.save_parameters + [params]
+        return super().on_after_backward()
 
     def configure_optimizers(self):
         return SGHMC(self.model.parameters(), lr=self.lr)

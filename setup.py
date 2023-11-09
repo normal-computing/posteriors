68d72f18868d5275d4af2f9267b7dfae4974d15d
johnathanchiu <johnathanchiu@berkeley.edu>
2023-11-09 20:04:42 +0000

    Add setup file to pip install uqlib

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="uqlib",
+    version="0.01",
+    packages=["uqlib"],
+    package_dir={
+        "uqlib": "./uqlib",
+    },
+    install_requires=[
+        # List your project dependencies here
+    ],
+)

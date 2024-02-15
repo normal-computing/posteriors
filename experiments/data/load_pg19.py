80c9400b414f3c9bc7c0e8ffa031d49b698fdd37
Sam Duffield <sam@normalcomputing.ai>
2024-02-15 11:52:17 +0000

    Load dataloaders function

---
+++ experiments/data/load_pg19.py
@@ -0,0 +1,100 @@
+from typing import Tuple
+from datasets import load_dataset
+from torch.utils.data import DataLoader, Dataset
+
+
+class DictDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(next(iter(self.data.values())))
+
+    def __getitem__(self, idx):
+        return {k: v[idx] for k, v in self.data.items()}
+
+
+def load_pg19_dataloaders(config, tokenizer) -> Tuple[DataLoader, DataLoader]:
+    dataset = load_dataset("json", data_files=config.dataset_path)["train"]
+
+    def split_doc(examples):
+        text_as_list = examples["text"].split()
+        start_index = int(len(text_as_list) * 0.85)
+        return {
+            "train_text": " ".join(text_as_list[:start_index]),
+            "test_text": " ".join(text_as_list[start_index:]),
+        }
+
+    def tokenize_and_stride(example_text):
+        tokenized_text = tokenizer(
+            example_text,
+            truncation=True,
+            max_length=config.stride_length,
+            stride=config.stride_overlap,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length",
+            return_tensors="pt",
+        )
+        return {
+            "input_ids": tokenized_text["input_ids"],
+            "attention_mask": tokenized_text["attention_mask"],
+        }
+
+    def tokenize_function(examples):
+        train_input_ids_and_mask = tokenize_and_stride(examples["train_text"])
+        test_input_ids_and_mask = tokenize_and_stride(examples["test_text"])
+        return {
+            "train_input_ids": train_input_ids_and_mask["input_ids"],
+            "train_attention_mask": train_input_ids_and_mask["attention_mask"],
+            "test_input_ids": test_input_ids_and_mask["input_ids"],
+            "test_attention_mask": test_input_ids_and_mask["attention_mask"],
+        }
+
+    split_datasets = dataset.map(split_doc, batched=False)
+    tokenized_datasets = split_datasets.map(tokenize_function, batched=False)
+    tokenized_datasets = tokenized_datasets.remove_columns(
+        [
+            "text",
+            "train_text",
+            "test_text",
+            "url",
+            "short_book_title",
+            "publication_date",
+        ]
+    )
+    tokenized_datasets.set_format("torch")
+
+    train_datasets = [
+        tokenized_datasets.remove_columns(["test_input_ids", "test_attention_mask"])
+        .rename_column("train_input_ids", "input_ids")
+        .rename_column("train_attention_mask", "attention_mask")[i]
+        for i in range(config.num_tasks)
+    ]
+    test_datasets = [
+        tokenized_datasets.remove_columns(["train_input_ids", "train_attention_mask"])
+        .rename_column("test_input_ids", "input_ids")
+        .rename_column("test_attention_mask", "attention_mask")[i]
+        for i in range(config.num_tasks)
+    ]
+
+    train_dataloaders = [
+        DataLoader(
+            DictDataset(train_ds),
+            batch_size=config.batch_size,
+            shuffle=False,
+            num_workers=config.num_workers,
+        )
+        for train_ds in train_datasets
+    ]
+
+    test_dataloaders = [
+        DataLoader(
+            DictDataset(test_ds),
+            batch_size=config.batch_size,
+            shuffle=False,
+            num_workers=config.num_workers,
+        )
+        for test_ds in test_datasets
+    ]
+    return train_dataloaders, test_dataloaders
--- experiments/single_laplace_lora.py
+++ experiments/single_laplace_lora.py
@@ -1,19 +1,11 @@
-import torch
-import pickle
 import os
 from transformers import AutoTokenizer
-
 from experiments.utils import load_config
-from experiments.laplace_lora import BayesTransformerModule
+from experiments.data.load_pg19 import load_pg19_dataloaders
 
-config = load_config("experiments/utils/configs/multiple_lora.yaml")
+config = load_config("experiments/utils/configs/lora_sam.yaml")
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-
-with open(config.dataset_path, "rb") as f:
-    dataset = pickle.load(f)
-
 
 tokenizer = AutoTokenizer.from_pretrained(
     config.model_config.pretrained_model_name_or_path
@@ -21,49 +13,6 @@ tokenizer = AutoTokenizer.from_pretrained(
 
 tokenizer.pad_token = tokenizer.eos_token
 
-
-with open(config.dataset_path, "rb") as f:
-    dataset = pickle.load(f)
-
-
-def split_doc(lst):
-    start_index = int(len(lst) * 0.85)
-    return lst[:start_index], lst[start_index:]
-
-
-for sample in dataset:
-    sample["input_ids"] = tokenizer(
-        sample["text"],
-        padding="max_length",
-        max_length=config.max_length,
-        truncation=True,
-    )["input_ids"]
-
-
-samples_per_task = [
-    dataset[(i * config.num_samples_per_task) : ((i + 1) * config.num_samples_per_task)]
-    for i in range(config.num_tasks)
-]
-
-split_samples = [
-    [split_doc(sample["input_ids"]) for sample in samples]
-    for samples in samples_per_task
-]
-
-
-train_datasets = [[sample[0] for sample in samples] for samples in split_samples]
-test_datasets = [[sample[1] for sample in samples] for samples in split_samples]
-
-train_dataloaders = [
-    torch.utils.data.DataLoader(
-        train,
-        shuffle=False,
-        batch_size=config.batch_size,
-        num_workers=config.num_workers,
-    )
-    for train in train_datasets
-]
+train_dataloaders, test_dataloaders = load_pg19_dataloaders(config, tokenizer)
 
 batch = next(iter(train_dataloaders[0]))
-
-model = BayesTransformerModule(config.model_config)

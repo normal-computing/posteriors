ea57cb0269df5d27630eed7cfbbe9f54fb2a9372
KaelanDt <kaelandonatella@gmail.com>
2024-03-05 15:33:16 +0000

    add support for full fisher laplace

---
+++ tests/laplace/test_full_fisher.py
@@ -0,0 +1,136 @@
+from functools import partial
+import torch
+import torch.nn as nn
+from torch.distributions import Normal
+from torch.utils.data import DataLoader, TensorDataset
+from torch.func import functional_call
+from optree import tree_map
+from uqlib.utils import tree_size
+from optree.integration.torch import ravel_pytree
+
+from uqlib.laplace import full_fisher
+
+class TestModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.device = "cpu"
+        self.linear = nn.Linear(4, 1)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+# Note this won't work if you replace torch.stack with torch.tensor
+def normal_log_prior(p: dict):
+    return torch.sum(
+        torch.stack([Normal(0, 1).log_prob(ptemp).sum() for ptemp in p.values()])
+    )
+
+
+def normal_log_likelihood(y, y_pred):
+    return (
+        Normal(y_pred, 1, validate_args=False).log_prob(y).sum(dim=-1)
+    )  # validate args introduces control flows not yet supported in torch.func.vmap
+
+
+# def normal_log_likelihood(y, y_pred):
+#     return (y - y_pred).square().sum(dim=-1)
+
+
+def log_posterior_n(params, batch, model, n_data):
+    y_pred = functional_call(model, params, batch[0])
+    return normal_log_prior(params) + normal_log_likelihood(
+        batch[1], y_pred
+    ) * n_data, torch.tensor([])
+
+
+def test_full_fisher_vmap():
+    torch.manual_seed(42)
+    model = TestModel()
+
+    xs = torch.randn(10, 4)
+    ys = model(xs)
+
+    dataloader = DataLoader(
+        TensorDataset(xs, ys),
+        batch_size=4,
+    )
+
+    def log_posterior(p, b):
+        return log_posterior_n(p, b, model, len(xs))[0].mean(), torch.tensor([])
+
+    params = dict(model.named_parameters())
+
+    transform = full_fisher.build(log_posterior)
+    laplace_state = transform.init(params)
+    for batch in dataloader:
+        laplace_state = transform.update(laplace_state, batch)
+        #print(laplace_state.prec)
+
+    print("finished full fisher")
+    num_params = tree_size(params)
+    expected = torch.zeros((num_params, num_params))
+    for x, y in zip(xs, ys):
+        x = x.unsqueeze(0)
+        y = y.unsqueeze(0)
+        with torch.no_grad():
+            jac, _ =  torch.func.jacrev(log_posterior, has_aux=True)(params, (x, y))
+            flat_jac, _ = ravel_pytree(jac)
+            flat_jac = flat_jac.reshape(x.shape[0], -1)
+            fisher = flat_jac.T @ flat_jac
+        expected += fisher
+
+    print(expected)
+    print(laplace_state.prec)
+    assert torch.allclose(expected, laplace_state.prec, atol=1e-4)
+
+    # Also check full batch
+    laplace_state_fb = transform.init(params)
+    #print(laplace_state_fb.prec)
+    laplace_state_fb = transform.update(laplace_state_fb, (xs, ys))
+    print(laplace_state_fb.prec)
+
+    assert torch.allclose(expected, laplace_state_fb.prec, atol=1e-4)
+
+    #  Test per_sample
+    log_posterior_per_sample = partial(log_posterior_n, model=model, n_data=len(xs))
+    transform_ps = full_fisher.build(log_posterior_per_sample, per_sample=True)
+    laplace_state_ps = transform_ps.init(params)
+    for batch in dataloader:
+        laplace_state_ps = transform_ps.update(
+            laplace_state_ps,
+            batch,
+        )
+
+  
+    assert torch.allclose(
+            laplace_state_ps.prec, laplace_state_fb.prec, atol=1e-5
+    )
+
+    # Test inplace
+    laplace_state_ip = transform.init(params)
+    laplace_state_ip2 = transform.update(
+        laplace_state_ip,
+        batch,
+        inplace=True,
+    )
+
+
+    assert torch.allclose(
+        laplace_state_ip2.prec, laplace_state_ip.prec, atol=1e-8
+    )
+
+    # Test not inplace
+    laplace_state_ip_false = transform.update(
+        laplace_state_ip,
+        batch,
+        inplace=False,
+    )
+
+    assert not torch.allclose(
+            laplace_state_ip_false.prec,
+            laplace_state_ip.prec,
+            atol=1e-8,
+        )
+
+test_full_fisher_vmap()
---
+++ uqlib/laplace/full_fisher.py
@@ -0,0 +1,128 @@
+from typing import Any
+from dataclasses import dataclass
+from functools import partial
+import torch
+from torch.func import jacrev
+from uqlib.types import TensorTree, Transform, LogProbFn, Tensor
+from optree.integration.torch import ravel_pytree
+from uqlib.utils import per_samplify, tree_size
+
+@dataclass
+class FullLaplaceState:
+    """State encoding a Normal distribution over parameters,
+    with a dense precision matrix
+    Args:
+        mean: Mean of the Normal distribution.
+        prec: Precision matrix of the Normal distribution.
+        aux: Auxiliary information from the log_posterior call.
+    """
+    mean: TensorTree
+    prec: Tensor
+    aux: Any = None
+
+def init(
+    params: TensorTree,
+    init_prec: Tensor | None = None,
+) -> FullLaplaceState:
+    """Initialise Normal distribution over parameters,
+    with a dense precision matrix.
+    Args:
+        params: Mean of the Normal distribution.
+        init_prec: Initial precision matrix. Defaults to identity.
+    Returns:
+        Initial FullLaplaceState.
+    """
+
+    if init_prec is None:
+        num_params = tree_size(params)
+        init_prec = torch.zeros((num_params, num_params), requires_grad=False)
+
+    return FullLaplaceState(params, init_prec)
+
+def update(
+    state: FullLaplaceState,
+    batch: Any,
+    log_posterior: LogProbFn,
+    per_sample: bool = False,
+    inplace: bool = True,
+) -> FullLaplaceState:
+    """Adds empirical Fisher information matrix of covariance summed over
+    given batch.
+
+    Args:
+        state: Current state.
+        batch: Input data to log_posterior.
+        log_posterior: Function that takes parameters and input batch and
+            returns the log posterior value (which can be unnormalised)
+        per_sample: If True, then log_posterior is assumed to return a vector of
+        log posteriors for each sample in the batch. If False, then log_posterior
+        is assumed to return a scalar log posterior for the whole batch, in this
+        case torch.func.vmap will be called, this is typically slower than
+        directly writing log_posterior to be per sample.
+        inplace: If True, the state is updated in place. Otherwise, a new
+            state is returned.
+    
+    Returns:
+        Updated FullLaplaceState.
+    """
+    if not per_sample:
+        log_posterior = per_samplify(log_posterior)
+    
+    with torch.no_grad():
+        
+        jac, aux = jacrev(log_posterior, has_aux=True)(state.mean, batch)
+        flat_jac, _ = ravel_pytree(jac)
+        flat_jac = flat_jac.reshape(batch[0].shape[0], -1)
+        fisher = flat_jac.T @ flat_jac
+    
+    if inplace:
+        state.prec += fisher
+        state.aux = aux
+        return state
+    else:
+        return FullLaplaceState(state.mean, state.prec + fisher, aux)
+
+
+def build(
+    log_posterior: LogProbFn,
+    per_sample: bool = False,
+    init_prec: Tensor | None = None,
+) -> Transform:
+    """Builds a transform for dense empirical Fisher information
+    Laplace approximation.
+
+    Args:
+        log_posterior: Function that takes parameters and input batch and
+            returns the log posterior value (which can be unnormalised)
+            as well as auxiliary information, e.g. from the model call.
+        per_sample: If True, then log_posterior is assumed to return a vector of
+            log posteriors for each sample in the batch. If False, then log_posterior
+            is assumed to return a scalar log posterior for the whole batch, in this
+            case torch.func.vmap will be called, this is typically slower than
+            directly writing log_posterior to be per sample.
+        init_prec_diag: Initial diagonal of the precision matrix. Defaults to zero.
+
+    Returns:
+        Diagonal empirical Fisher information Laplace approximation transform
+        (uqlib.types.Transform instance).
+    """
+    init_fn = partial(init, init_prec=init_prec)
+    update_fn = partial(update, log_posterior=log_posterior, per_sample=per_sample)
+    return Transform(init_fn, update_fn)
+
+def sample(
+    state: FullLaplaceState,
+    sample_shape: torch.Size = torch.Size([]),
+) -> Tensor:
+    """Sample from Normal distribution over parameters.
+
+    Args:
+        state: State encoding mean and precision matrix.
+        sample_shapa: Shape of the desired samples.
+
+    Returns:
+        Sample(s) from the Normal distribution.
+    """
+    return torch.distributions.MultivariateNormal(loc=state.mean, 
+                                                  precision_matrix=state.prec).sample(sample_shape
+                                                    )

fix empirical fisher calculation and add function to utils

tests/laplace/test_full_fisher.py
@@ -4,8 +4,7 @@ import torch.nn as nn
 from torch.distributions import Normal
 from torch.utils.data import DataLoader, TensorDataset
 from torch.func import functional_call
-from uqlib.utils import tree_size
-from optree.integration.torch import ravel_pytree
+from uqlib.utils import tree_size, empirical_fisher
 
 from uqlib.laplace import full_fisher
 
@@ -14,7 +13,7 @@ class TestModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.device = "cpu"
-        self.linear = nn.Linear(4, 1)
+        self.linear = nn.Linear(10, 1)
 
     def forward(self, x):
         return self.linear(x)
@@ -48,12 +47,12 @@ def test_full_fisher_vmap():
     torch.manual_seed(42)
     model = TestModel()
 
-    xs = torch.randn(10, 4)
+    xs = torch.randn(100, 10)
     ys = model(xs)
 
     dataloader = DataLoader(
         TensorDataset(xs, ys),
-        batch_size=4,
+        batch_size=2,
     )
 
     def log_posterior(p, b):
@@ -65,32 +64,24 @@ def test_full_fisher_vmap():
     laplace_state = transform.init(params)
     for batch in dataloader:
         laplace_state = transform.update(laplace_state, batch)
-        # print(laplace_state.prec)
 
-    print("finished full fisher")
     num_params = tree_size(params)
     expected = torch.zeros((num_params, num_params))
     for x, y in zip(xs, ys):
         x = x.unsqueeze(0)
         y = y.unsqueeze(0)
         with torch.no_grad():
-            jac, _ = torch.func.jacrev(log_posterior, has_aux=True)(params, (x, y))
-            flat_jac, _ = ravel_pytree(jac)
-            flat_jac = flat_jac.reshape(x.shape[0], -1)
-            fisher = flat_jac.T @ flat_jac
+            fisher = empirical_fisher(log_posterior, params, (x, y))[0]
+
         expected += fisher
 
-    print(expected)
-    print(laplace_state.prec)
-    assert torch.allclose(expected, laplace_state.prec, atol=1e-4)
+    assert torch.allclose(expected, laplace_state.prec, atol=1e-5)
 
     # Also check full batch
     laplace_state_fb = transform.init(params)
-    # print(laplace_state_fb.prec)
     laplace_state_fb = transform.update(laplace_state_fb, (xs, ys))
-    print(laplace_state_fb.prec)
 
-    assert torch.allclose(expected, laplace_state_fb.prec, atol=1e-4)
+    assert torch.allclose(expected, laplace_state_fb.prec, atol=1e-5)
 
     #  Test per_sample
     log_posterior_per_sample = partial(log_posterior_n, model=model, n_data=len(xs))
@@ -126,6 +117,3 @@ def test_full_fisher_vmap():
         laplace_state_ip.prec,
         atol=1e-8,
     )
-
-
-test_full_fisher_vmap()

uqlib/laplace/full_fisher.py
@@ -2,10 +2,8 @@ from typing import Any
 from dataclasses import dataclass
 from functools import partial
 import torch
-from torch.func import jacrev
 from uqlib.types import TensorTree, Transform, LogProbFn, Tensor
-from optree.integration.torch import ravel_pytree
-from uqlib.utils import per_samplify, tree_size
+from uqlib.utils import per_samplify, tree_size, empirical_fisher
 
 
 @dataclass
@@ -72,11 +70,7 @@ def update(
     if not per_sample:
         log_posterior = per_samplify(log_posterior)
 
-    with torch.no_grad():
-        jac, aux = jacrev(log_posterior, has_aux=True)(state.mean, batch)
-        flat_jac, _ = ravel_pytree(jac)
-        flat_jac = flat_jac.reshape(batch[0].shape[0], -1)
-        fisher = flat_jac.T @ flat_jac
+    fisher, aux = empirical_fisher(log_posterior, state.mean, batch)
 
     if inplace:
         state.prec += fisher

uqlib/utils.py
@@ -5,7 +5,7 @@ from torch.func import grad, jvp, functional_call, jacrev
 from torch.distributions import Normal
 from optree import tree_map, tree_map_, tree_reduce, tree_flatten
 
-from uqlib.types import TensorTree, ForwardFn
+from uqlib.types import TensorTree, ForwardFn, Tensor
 
 
 def model_to_function(model: torch.nn.Module) -> Callable[[TensorTree, Any], Any]:
@@ -475,3 +475,16 @@ def is_scalar(x: Any) -> bool:
         True if x is a scalar.
     """
     return isinstance(x, (int, float)) or (torch.is_tensor(x) and x.numel() == 1)
+
+
+def empirical_fisher(
+    f: Callable[[TensorTree, TensorTree], Any], params: TensorTree, batch: Any
+) -> Tensor:
+    with torch.no_grad():
+        batch_jac, aux = jacrev(f, has_aux=True)(params, batch)
+        batched_params = [
+            batch_jac[key].reshape(batch_jac[key].shape[0], -1) for key in batch_jac
+        ]
+        flat_jac = torch.cat(batched_params, dim=1)
+        fisher = flat_jac.T @ flat_jac
+    return fisher, aux
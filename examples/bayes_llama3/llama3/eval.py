3f218f9432be716a68e1347da389e6bc8a637a25
johnathanchiu <johnathanchiu@berkeley.edu>
2024-05-21 19:06:51 +0000

    clean up eval code for statement completion

--- examples/bayes_llama3/llama3/eval.py
+++ examples/bayes_llama3/llama3/eval.py
@@ -1,17 +1,17 @@
-import json
 import os
-import pickle
 
 import torch
-from tqdm import tqdm
+import torch.nn.functional as F
 from ml_collections.config_dict import ConfigDict, FrozenConfigDict
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import numpy as np
 
+from llama3.data.statements import scientific_statements, scientific_statements_samoan
 from llama3.modules.bayesllama import BayesLlamaForCausalLM
 from llama3.utils.load_utils import load_ensemble
 
 
-PROMPT = """Answer the following multiple choice question very succintly.\n"""
+PROMPT = "Complete the following statement:\n"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 BATCH_SIZE = 1
@@ -26,40 +26,9 @@ def logits_to_uncertainties(eprobs):
     return total_uncertainty, epistemic_uncertainty
 
 
-def extract_tqa_questions(sample):
-    all_qs = []
-    for key, val in sample["questions"]["nonDiagramQuestions"].items():
-        correct_answer = val["correctAnswer"]["processedText"]
-        if correct_answer not in val["answerChoices"]:
-            # Adding catch
-            continue
-        correct_answer = val["answerChoices"][correct_answer]["rawText"]
-        question = val["beingAsked"]["processedText"]
-        for _, mc in val["answerChoices"].items():
-            question += "\n" + mc["idStructural"] + " " + mc["processedText"]
-        all_qs.append((key, question, correct_answer))
-    return all_qs
-
-
-def extract_head_questions(sample):
-    all_qs = []
-    for q in sample["data"]:
-        question = q["qtext"]
-        for mc in q["answers"]:
-            question += "\n" + f"{str(mc['aid'])}." + " " + mc["atext"]
-
-        for answer in q["answers"]:
-            if str(answer["aid"]) == str(q["ra"]):
-                atext = answer["atext"]
-        correct_answer = f"{str(q['ra'])}." + " " + atext
-        all_qs.append((q["qid"], question, correct_answer))
-    return all_qs
-
-
-class Experiment:
+class EvaluationEngine:
     def __init__(self, config: FrozenConfigDict):
-        self.config = ConfigDict(config)  # thaw config
-        self.experiment_log_dir = config.experiment_log_dir
+        self.config = ConfigDict(config)
         self.n_tokens = config["n_tokens"]
 
         self.tokenizer = AutoTokenizer.from_pretrained(
@@ -92,25 +61,38 @@ class Experiment:
 
         self.model.to(DEVICE)
 
-    def prepare_prompt(self, prompt, questions):
-        prompt = ["\n".join([prompt, q + "\n", "Answer:\n"]) for q in questions]
-        return self.tokenizer(prompt, return_tensors="pt", padding=True)
-
     @torch.no_grad()
-    def generate(self, inputs, max_length=20, use_cache=True):
-        seq_out = []
+    def generate(self, inputs, gt_token, max_length=20, use_cache=True, is_base=False):
+        accuracy = 0.0
+        prediction_loss = []
         epistemic_uncertainties = [[] for _ in range(inputs["input_ids"].size(0))]
         total_uncertainties = [[] for _ in range(inputs["input_ids"].size(0))]
-        for _ in range(max_length):
+
+        predictions = []
+        for token_idx in range(max_length):
             outputs = self.model(**inputs, return_dict=False, use_cache=use_cache)
 
             if "attention_mask" in inputs:
                 del inputs["attention_mask"]
 
-            elogits = outputs[0][:, :, -1]
+            if is_base:
+                elogits = outputs[0][:, -1].unsqueeze(0)
+            else:
+                elogits = outputs[0][:, :, -1]
+
             eprobs = torch.softmax(elogits, dim=-1)
             probs = eprobs.mean(0)
+
+            # (batch size, vocab_size)
+            pred_logits = torch.log(probs)
             next_token = probs.argmax(-1).unsqueeze(-1)
+
+            if token_idx == 0:
+                accuracy += torch.where(next_token.squeeze(-1).cpu() == gt_token)[
+                    0
+                ].numel()
+                loss = F.cross_entropy(pred_logits.cpu(), gt_token)
+                prediction_loss.append(loss.item())
 
             total_unc, epi_unc = logits_to_uncertainties(eprobs.cpu())
             for idx, (tunc, eunc) in enumerate(zip(total_unc, epi_unc)):
@@ -119,128 +101,93 @@ class Experiment:
 
             if use_cache:
                 inputs["past_key_values"] = outputs[1]
-                inputs["ensemble_past_key_values"] = outputs[2]
+                if not is_base:
+                    inputs["ensemble_past_key_values"] = outputs[2]
                 inputs["input_ids"] = next_token
             else:
                 inputs["input_ids"] = torch.cat(
                     [inputs["input_ids"], next_token], dim=1
                 )
-            seq_out.append(next_token)
+            predictions.append(next_token)
 
-        seq_out = torch.cat(seq_out, -1)
-        text_outputs = self.tokenizer.batch_decode(seq_out, skip_special_tokens=True)
-        return text_outputs, total_uncertainties, epistemic_uncertainties
+        predictions = torch.cat(predictions, -1)
+        text_outputs = self.tokenizer.batch_decode(
+            predictions, skip_special_tokens=True
+        )
+        return (
+            text_outputs,
+            total_uncertainties,
+            epistemic_uncertainties,
+            prediction_loss,
+            accuracy,
+        )
 
-    @torch.no_grad()
-    def generate_base(self, inputs, max_length=20, use_cache=True):
-        seq_out = []
-        epistemic_uncertainties = [[] for _ in range(inputs["input_ids"].size(0))]
-        total_uncertainties = [[] for _ in range(inputs["input_ids"].size(0))]
-        for _ in range(max_length):
-            outputs = self.model(**inputs, return_dict=False, use_cache=use_cache)
+    def run_eval(self, statements, batch_size=3, n_tokens=5):
+        model_outputs = []
+        accuracies = 0.0
+        uncertainties = {"epistemic": [], "total": []}
+        loss_metrics = []
+        for i in range(0, len(statements), batch_size):
+            last_words = [
+                " " + s.split(" ")[-1] for s in statements[i : i + batch_size]
+            ]
+            batch_statements = [
+                " ".join(s.split(" ")[:-1]) for s in statements[i : i + batch_size]
+            ]
 
-            if "attention_mask" in inputs:
-                del inputs["attention_mask"]
+            self.tokenizer.padding_side = "right"
+            last_token = self.tokenizer(last_words, padding=True, return_tensors="pt")[
+                "input_ids"
+            ][:, 1]
+            self.tokenizer.padding_side = "left"
 
-            logits = outputs[0][:, -1]
-            probs = torch.softmax(logits, dim=-1)
-            next_token = probs.argmax(-1).unsqueeze(-1)
+            inputs = self.tokenizer(
+                [PROMPT + s + " " for s in batch_statements],
+                padding=True,
+                return_tensors="pt",
+            )
+            (
+                text_outputs,
+                total_uncertainty,
+                epistemic_uncertainty,
+                batch_loss,
+                batch_accuracy,
+            ) = self.generate(
+                inputs.to("cuda"),
+                last_token,
+                max_length=n_tokens,
+                use_cache=True,
+                is_base=self.eval_pretrained,
+            )
+            loss_metrics.extend(batch_loss)
+            accuracies += batch_accuracy
 
-            total_unc, epi_unc = logits_to_uncertainties(probs.unsqueeze(0).cpu())
-            for idx, (tunc, eunc) in enumerate(zip(total_unc, epi_unc)):
-                total_uncertainties[idx].append(tunc.item())
-                epistemic_uncertainties[idx].append(eunc.item())
+            for text_output, total_unc, epi_unc in zip(
+                text_outputs, total_uncertainty, epistemic_uncertainty
+            ):
+                model_outputs.append(text_output)
+                uncertainties["total"].append(total_unc)
+                uncertainties["epistemic"].append(epi_unc)
 
-            if use_cache:
-                inputs["past_key_values"] = outputs[1]
-                inputs["input_ids"] = next_token
-            else:
-                inputs["input_ids"] = torch.cat(
-                    [inputs["input_ids"], next_token], dim=1
-                )
-            seq_out.append(next_token)
+        return (
+            model_outputs,
+            uncertainties,
+            np.average(loss_metrics),
+            accuracies / len(statements),
+        )
 
-        seq_out = torch.cat(seq_out, -1)
-        text_outputs = self.tokenizer.batch_decode(seq_out, skip_special_tokens=True)
-        return text_outputs, total_uncertainties, epistemic_uncertainties
-
-    def run_experiment(self, dataset_name: str, dataset_path: str, split: str):
-        with open(dataset_path, "rb") as file:
-            dataset = json.load(file)
-        if dataset_name == "head_qa":
-            dataset = dataset["exams"].values()
+    def run(self):
+        statements = [
+            (scientific_statements, "en"),
+            (scientific_statements_samoan, "sa"),
+        ]
 
         results = {}
-        for sample_idx, sample in tqdm(list(enumerate(dataset))):
-            if dataset_name == "head_qa":
-                sample = extract_head_questions(sample)
-            else:
-                sample = extract_tqa_questions(sample)
-
-            for idx in range(0, len(sample), BATCH_SIZE):
-                batch_sample = sample[idx : idx + BATCH_SIZE]
-
-                q_ids = [q[0] for q in batch_sample]
-                questions = [q[1] for q in batch_sample]
-                correct_answers = [q[2] for q in batch_sample]
-
-                inputs = self.prepare_prompt(PROMPT, questions)
-
-                if self.eval_pretrained:
-                    answers, total_uncertainties, epistemic_uncertainties = (
-                        self.generate_base(
-                            inputs.to(self.model.device), max_length=self.n_tokens
-                        )
-                    )
-                else:
-                    answers, total_uncertainties, epistemic_uncertainties = (
-                        self.generate(
-                            inputs.to(self.model.device), max_length=self.n_tokens
-                        )
-                    )
-
-                for qid, answer, ca, q, tunc, eunc in zip(
-                    q_ids,
-                    answers,
-                    correct_answers,
-                    questions,
-                    total_uncertainties,
-                    epistemic_uncertainties,
-                ):
-                    results[qid] = {
-                        "question": q,
-                        "response": answer,
-                        "expected_response": ca,
-                        "total_uncertainty": tunc,
-                        "aleatoric_uncertainty": eunc,
-                    }
-
-            if sample_idx % 10 == 0:
-                self.save_results(dataset_name, results, split)
-
-        return results
-
-    def save_results(self, dataset, results, split):
-        """
-        Save results as pickle file
-        """
-        result_file = os.path.join(
-            self.experiment_log_dir, f"{dataset}_results_{split}.pkl"
-        )
-        with open(result_file, "wb") as f:
-            pickle.dump(results, f)
-
-    def run(self, dataset_name: str, dataset_path: str, **kwargs):
-        """
-        Run experiment
-        """
-        splits = os.path.basename(dataset_path).split(".")[0].split("_")
-        split = ""
-        if "train" in splits:
-            split = "train"
-        elif "test" in splits:
-            split = "test"
-        results = self.run_experiment(dataset_name, dataset_path, split, **kwargs)
-        self.save_results(dataset_name, results, split)
-
-        return results
+        for statement_list, lang in statements:
+            outputs, uncertainties, loss, acc = self.run_eval(statement_list)
+            results[lang] = {
+                "outputs": outputs,
+                "uncertainties": uncertainties,
+                "loss": loss,
+                "acc": acc,
+            }
--- examples/bayes_llama3/run_eval.py
+++ examples/bayes_llama3/run_eval.py
@@ -1,39 +1,17 @@
-"""Script for running perplexity experiment."""
+import argparse
+import pickle
 
-from absl import app, flags
+from llama3.eval import EvaluationEngine
+from llama3.utils.load_utils import load_config
 
-from llama3.eval import Experiment
-from llama3.utils.load_utils import load_config, save_config, setup_log_dir
+parser = argparse.ArgumentParser()
+parser.add_argument("--base", type=str, required=True)
+parser.add_argument("--output", type=str, required=True)
+args = parser.parse_args()
 
-FLAGS = flags.FLAGS
-flags.DEFINE_string("base", None, "Path to base config.")
-flags.DEFINE_string("devices", None, "Devices to use.")
-flags.DEFINE_boolean("verbose", False, "Whether to print non-flag arguments.")
+config = load_config(args.base)
+experiment = EvaluationEngine(config["experiment_config"])
+results = experiment.run_eval(n_tokens=1)
 
-
-def main(argv):
-    """
-    Main function for running experiments.
-    """
-    if FLAGS.verbose:
-        print("non-flag arguments:", argv)
-
-    assert FLAGS.base is not None, "Configs not specified, must specify base"
-    config = load_config(FLAGS.base)
-
-    experiment_log_dir = setup_log_dir(
-        config.get("logs_dir", "experiment_logs"),
-        experiment_name=config.get("experiment_name", None),
-    )
-
-    config["experiment_config"]["experiment_log_dir"] = experiment_log_dir
-    save_config(config.to_dict(), experiment_log_dir + "/config.yaml")
-
-    experiment = Experiment(config["experiment_config"])
-    for key, paths in config["dataset_path"].items():
-        for path in paths:
-            experiment.run(dataset_name=key, dataset_path=path)
-
-
-if __name__ == "__main__":
-    app.run(main)
+with open(args.output, "wb") as f:
+    pickle.dump(results, f)

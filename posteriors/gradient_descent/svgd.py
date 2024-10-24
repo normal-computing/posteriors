7008844b4be352c735db9e07b4fb955f4a86aaea
EtayLivne <etay.livne@gmail.com>
2024-10-24 17:08:07 +0300

    ruff changes

--- posteriors/gradient_descent/svgd.py
+++ posteriors/gradient_descent/svgd.py
@@ -1,7 +1,6 @@
 from functools import partial
 from typing import Callable, Any, NamedTuple
 
-import torchopt
 from optree import tree_map
 from torch.func import grad_and_value, vmap
 from optree.integration.torch import tree_ravel
@@ -10,50 +9,58 @@ from posteriors.types import TensorTree, Transform
 
 
 def _build_stein_variational_gradient_step(
-    log_posterior_gradient: Callable[[TensorTree, Any], TensorTree], kernel: Callable
+    log_posterior: Callable[[TensorTree, Any], TensorTree], kernel: Callable
 ):
     """
     hardcode a function that calculates phi_star according to user defined log_posterior_gradient
     """
 
-    def _phi_star_summand(param, param_, batch):
-        grad_log = log_posterior_gradient(param, batch)
-        k, grad_k = grad_and_value(kernel, argnums=0)(param, param_)
-        return tree_map(lambda gl, gk: -(k * gl) - gk, grad_log, grad_k)
+    # def _phi_star_summand(param, param_, batch):
+    #     log_prob_grad, _ = grad_and_value(log_posterior, argnums=0)(param, batch)
+    #     grad_k, k = grad_and_value(kernel, argnums=0)(param, param_)
+    #     return tree_map(lambda gl, gk: (k * gl) + gk, log_prob_grad, grad_k)
 
     def step(params: TensorTree, batch):
+        def _phi_star_summand(param, param_, batch):
+            log_prob_grad, _ = grad_and_value(log_posterior, argnums=0)(param, batch)
+            grad_k, k = grad_and_value(kernel, argnums=0)(param, param_)
+            return tree_map(lambda gl, gk: (k * gl) + gk, log_prob_grad, grad_k)
+
         phi_star_summand = partial(_phi_star_summand, batch=batch)
         r_params, unravel = tree_ravel(params)
-        gradients = vmap(
-            lambda param: (
-                vmap(lambda param_: phi_star_summand(param, param_))(r_params).mean(
-                    axis=0
-                )
-            )
-        )(r_params)
-        return unravel(gradients)
+
+        gradients = tree_map(
+            lambda p: vmap(lambda p_: phi_star_summand(p, p_))(r_params).mean(axis=0),
+            params,
+        )
+        return gradients
+        # gradients = vmap(
+        #     lambda param: (
+        #         vmap(lambda param_: phi_star_summand(param, param_))(r_params).mean(
+        #             axis=0
+        #         )
+        #     )
+        # )(r_params)
+        # return unravel(gradients)
 
     return step
 
 
 def build(
-    log_posterior_gradient: Callable[[TensorTree, Any], TensorTree],
-    optimizer: torchopt.base.GradientTransformation,
-    kernel: Callable[[TensorTree, Any], float],
+    log_posterior: Callable[[TensorTree, Any], TensorTree],
+    learning_rate: float,
+    kernel: Callable,
 ) -> Transform:
     """
     TBD
     """
-    init_fn = partial(init, optimizer=optimizer)
-    step_gradient_fn = _build_stein_variational_gradient_step(
-        log_posterior_gradient, kernel
-    )
+    step_gradient_fn = _build_stein_variational_gradient_step(log_posterior, kernel)
     update_fn = partial(
         update,
         step_function=step_gradient_fn,
-        optimizer=optimizer,
+        learning_rate=learning_rate,
     )
-    return Transform(init_fn, update_fn)
+    return Transform(init, update_fn)
 
 
 class SVGDState(NamedTuple):
@@ -62,23 +69,20 @@ class SVGDState(NamedTuple):
     """
 
     params: TensorTree
-    opt_state: torchopt.typing.OptState
 
 
 def init(
     params: TensorTree,
-    optimizer: torchopt.base.GradientTransformation,
 ) -> SVGDState:
     """TBD"""
-    opt_state = optimizer.init(params)
-    return SVGDState(params, opt_state)
+    return SVGDState(params)
 
 
 def update(
     state: SVGDState,
     batch: Any,
     step_function: Callable,
-    optimizer: torchopt.base.GradientTransformation,
+    learning_rate: float,
     inplace: bool = False,
 ) -> SVGDState:
     """
@@ -86,10 +90,8 @@ def update(
     """
 
     step_gradient = step_function(state.params, batch)
-
-    updates, opt_state = optimizer.update(
-        step_gradient, state.opt_state, params=state.params, inplace=inplace
-    )
-    params = torchopt.apply_updates(state.params, updates)
-
-    return SVGDState(params, opt_state)
+    params = tree_map(lambda p, g: p + learning_rate * g, state.params, step_gradient)
+    if inplace:
+        state.params = params
+    else:
+        return SVGDState(params)

532102cea9a831145482c9ff4b729028de750263
Sam Duffield <s@mduffield.com>
2025-02-17 14:25:12 +0000

    Amend types.py

--- posteriors/types.py
+++ posteriors/types.py
@@ -1,46 +1,26 @@
-from typing import Protocol, Any, TypeAlias, Tuple, Callable, NamedTuple
+from typing import (
+    Protocol,
+    Any,
+    TypeAlias,
+    Tuple,
+    Callable,
+    NamedTuple,
+)
 from optree.typing import PyTreeTypeVar
-from optree import registry
 from torch import Tensor
 
-TensorTree: TypeAlias = PyTreeTypeVar("TensorTree", Tensor)  # type: ignore
+TensorTree: TypeAlias = PyTreeTypeVar("TensorTree", Tensor)
 
 LogProbFn = Callable[[TensorTree, TensorTree], Tuple[float, TensorTree]]
 ForwardFn = Callable[[TensorTree, TensorTree], Tuple[Tensor, TensorTree]]
 OuterLogProbFn = Callable[[TensorTree, TensorTree], float]
-
-namespace = registry.__GLOBAL_NAMESPACE
-
-
-class TransformState(NamedTuple):
-    """A `posteriors` transform state is a `NamedTuple` containing the required
-    information for the posteriors iterative algorithm defined by the `init` and
-    `update` functions.
-
-    Inherit the `NamedTuple` class when defining a new algorithm state, just make sure
-    to include the `params` and `aux` fields.
-
-    ```
-    class AlgorithmState(NamedTuple):
-        params: TensorTree
-        algorithm_info: Any
-        aux: Any
-    ```
-
-    Attributes:
-        params: PyTree containing the current value of parameters.
-        aux: Auxiliary information from the model call.
-    """
-
-    params: TensorTree
-    aux: Any
 
 
 class InitFn(Protocol):
     @staticmethod
     def __call__(
         params: TensorTree,
-    ) -> TransformState:
+    ) -> TensorTree:
         """Initiate a posteriors state with unified API:
 
         ```
@@ -48,7 +28,7 @@ class InitFn(Protocol):
         ```
 
         where params is a PyTree of parameters. The produced `state` is a
-        `TransformState` (`NamedTuple`) containing the required information for the
+        `tensordict.tensorclass` containing the required information for the
         posteriors iterative algorithm defined by the `init` and `update` functions.
 
         Note that this represents the `init` function as stored in a `Transform`
@@ -59,24 +39,26 @@ class InitFn(Protocol):
             params: PyTree containing initial value of parameters.
 
         Returns:
-            The initial state (`NamedTuple`).
+            The initial state, a `tensordict.tensorclass` with `params` and `aux`
+            attributes but possibly other attributes too.
         """
+        ...
 
 
 class UpdateFn(Protocol):
     @staticmethod
     def __call__(
-        state: TransformState,
+        state: TensorTree,
         batch: Any,
         inplace: bool = False,
-    ) -> TransformState:
+    ) -> TensorTree:
         """Transform a posteriors state with unified API:
 
         ```
         state = update(state, batch, inplace=False)
         ```
 
-        where state is a `NamedTuple` containing the required information for the
+        where state is a `tensordict.tensorclass` containing the required information for the
         posteriors iterative algorithm defined by the `init` and `update` functions.
 
         Note that this represents the `update` function as stored in a `Transform`
@@ -84,13 +66,16 @@ class UpdateFn(Protocol):
         the algorithm module can and likely will have additional arguments.
 
         Args:
-            state: The state of the iterative algorithm.
+            state: The `tensordict.tensorclass` state of the iterative algorithm.
             batch: The data batch.
             inplace: Whether to modify state using inplace operations. Defaults to True.
 
         Returns:
-            The transformed state (`NamedTuple`).
+            The transformed state, a `tensordict.tensorclass` with `params` and `aux`
+            attributes but possibly other attributes too. Must be of the same type as
+            the input state.
         """
+        ...
 
 
 class Transform(NamedTuple):

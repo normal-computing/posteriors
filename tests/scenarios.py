421ab42d84a073c3818626d02abbc5ec23b04552
Sam Duffield <s@mduffield.com>
2025-03-12 16:08:57 +0000

    Unify mcmc tests

--- tests/scenarios.py
+++ tests/scenarios.py
@@ -1,10 +1,11 @@
-from typing import Any, Tuple
+from typing import Any
 import torch
 import torch.nn as nn
-from optree import tree_map
+from torch.distributions import MultivariateNormal
+from optree import tree_leaves
+from optree.integration.torch import tree_ravel
 
-from posteriors.utils import diag_normal_log_prob, diag_normal_sample
-from posteriors.types import TensorTree
+from posteriors.types import TensorTree, Transform, LogProbFn
 
 
 class TestModel(nn.Module):
@@ -38,32 +39,85 @@ class TestLanguageModel(nn.Module):
         return {"logits": logits}
 
 
-def batch_normal_log_prob(
-    p: dict, batch: Any, mean: dict, sd_diag: dict
-) -> Tuple[torch.Tensor, TensorTree]:
-    return diag_normal_log_prob(p, mean, sd_diag), torch.tensor([])
+def get_multivariate_normal_log_prob(
+    dim: int,
+) -> tuple[LogProbFn, tuple[torch.Tensor, torch.Tensor]]:
+    mean = torch.randn(dim)
+    sqrt_cov = torch.randn(dim, dim)
+    cov = sqrt_cov @ sqrt_cov.T
+    chol_cov = torch.linalg.cholesky(cov)  # Lower triangular with positive diagonal
+
+    def log_prob(p, batch):
+        p_flat = tree_ravel(p)[0]
+        lp = MultivariateNormal(mean, scale_tril=chol_cov).log_prob(p_flat)
+        return lp, torch.tensor([])
+
+    return log_prob, (mean, cov)
 
 
-def gaussian_mixture_log_prob(
-    p: dict, batch: Any, means: dict, diag_sds: dict, weights: dict
-) -> Tuple[torch.Tensor, TensorTree]:
-    log_mixture_probs = torch.vmap(diag_normal_log_prob, in_dims=(None, 0, 0))(
-        p, means, diag_sds
-    )
-    return torch.logsumexp(log_mixture_probs + torch.log(weights)), torch.tensor([])
-
-
-def gaussian_mixture_sample(
-    means: TensorTree, diag_sds: TensorTree, weights: torch.Tensor, n_samples: int
+def run_transform(
+    transform: Transform,
+    params: TensorTree,
+    n_steps: int,
 ) -> TensorTree:
-    component_indices = torch.multinomial(weights, n_samples, replacement=True)
+    state = transform.init(params)
 
-    def get_mean(i):
-        return tree_map(lambda x: x[i], means)
+    all_states = torch.unsqueeze(state, 0)
 
-    def get_sd(i):
-        return tree_map(lambda x: x[i], diag_sds)
+    for _ in range(n_steps):
+        state, _ = transform.update(state, None)
+        all_states = torch.cat((all_states, torch.unsqueeze(state, 0)), dim=0)
 
-    return tree_map(
-        lambda i: diag_normal_sample(get_mean(i), get_sd(i)), component_indices
+    return all_states
+
+
+def cumulative_mean_and_cov(xs: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    n, d = xs.shape
+    out_means = torch.zeros((n, d))
+    out_covs = torch.zeros((n, d, d))
+
+    out_means[0] = xs[0]
+    out_covs[0] = torch.eye(d)
+
+    for i in range(1, n):
+        n = i + 1
+        out_means[i] = out_means[i - 1] * n / (n + 1) + xs[i] / (n + 1)
+
+        delta_n = xs[i] - out_means[i - 1]
+        out_covs[i] = (
+            out_covs[i - 1] * (n - 2) / (n - 1) + torch.outer(delta_n, delta_n) / n
+        )
+
+    return out_means, out_covs
+
+
+def kl_gaussians(
+    m0: torch.Tensor, c0: torch.Tensor, m1: torch.Tensor, c1: torch.Tensor
+) -> torch.Tensor:
+    return 0.5 * (
+        torch.trace(c1.inverse() @ c0)
+        + torch.dot(m1 - m0, c1.inverse() @ (m1 - m0))
+        - len(m0)
+        + torch.log(torch.linalg.det(c1) / torch.linalg.det(c0))
     )
+
+
+def verify_inplace_update(transform: Transform, params: TensorTree, batch: Any):
+    init_state = transform.init(params)
+
+    # One step not inplace
+    torch.manual_seed(42)
+    state_not_inplace, _ = transform.update(init_state, batch)
+
+    # One step in place = same as not inplace
+    torch.manual_seed(42)
+    state_inplace, _ = transform.update(init_state, batch, inplace=True)
+
+    # Iterate through leaves of params and check they are the same
+    for init_leaf, not_inplace_leaf, inplace_leaf in zip(
+        tree_leaves(init_state.to_dict()),
+        tree_leaves(state_not_inplace.to_dict()),
+        tree_leaves(state_inplace.to_dict()),
+    ):
+        assert torch.equal(inplace_leaf, not_inplace_leaf)
+        assert torch.equal(init_leaf, not_inplace_leaf)
---
+++ tests/sgmcmc/utils.py
@@ -0,0 +1,51 @@
+from typing import Callable
+import torch
+from posteriors.sgmcmc import baoa
+from posteriors.types import LogProbFn, Transform
+from tests import scenarios
+
+
+def run_test_mcmc_gaussian(
+    build_transform: Callable[[LogProbFn], Transform],
+    dim: int = 5,
+    n_steps: int = 10_000,
+    burnin: int = 1_000,
+):
+    torch.manual_seed(42)
+
+    # Load log posterior
+    log_prob, (mean, cov) = scenarios.get_multivariate_normal_log_prob(dim)
+
+    # Build transform
+    transform = build_transform(log_prob)
+
+    # Initialise parameters
+    params = torch.randn(dim)
+
+    # Run transform
+    all_states = scenarios.run_transform(transform, params, n_steps)
+
+    # Remove burnin
+    all_states = all_states[burnin:]
+
+    # SGMCMC can be quite noisy, so we'll check the KL distance between the true and
+    # inferred sample Gaussian decreases as we get more samples.
+    cumulative_mean, cumulative_cov = scenarios.cumulative_mean_and_cov(
+        all_states.params
+    )
+    kl_divs = torch.func.vmap(scenarios.kl_gaussians, in_dims=(0, 0, None, None))(
+        cumulative_mean, cumulative_cov, mean, cov
+    )
+    # Check generally decreasing by checking n_check equally spaced KL divs
+    start_num_samples = 10
+    n_check = 5
+    cum_check_inds = torch.linspace(start_num_samples, n_steps, n_check)
+    cum_check_inds = cum_check_inds.int()
+
+    def get_kl_div(ind):
+        m = all_states.params[:ind].mean(0)
+        c = all_states.params[:ind].T.cov()
+        return scenarios.kl_gaussians(m, c, mean, cov)
+
+    kl_divs = torch.stack([get_kl_div(ind) for ind in cum_check_inds])
+    assert torch.all(kl_divs[:-1] > kl_divs[1:])
